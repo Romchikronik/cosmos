3ddb44f9a9f1d6048a63e0c5ed99502d3873af23
Romchik <romankolesov44@gmail.com>
2022-08-23 21:12:42 +0500

    Update text-changer.js

--- js/text-changer.js
+++ js/text-changer.js
@@ -1,13 +1,16 @@
-	$(document).ready(function() {
-  var str = [" vps server .                        ",
-             " web hosting .                        ",
-			 " domains .                        "];
-  var pos = 0, a = 0;
+$(document).ready(function () {
+  var str = [
+    " преподавателя .                        ",
+    " репетитора .                        ",
+    " наставника .                        ",
+  ];
+  var pos = 0,
+    a = 0;
   var html = "";
   function displayText() {
     if (pos > 2) pos = 0;
-    console.log(pos);
-    console.log(str[pos].length);
+    // console.log(pos);
+    // console.log(str[pos].length);
     if (a < str[pos].length) {
       html += str[pos].charAt(a);
       $(".text-change").html(html);
