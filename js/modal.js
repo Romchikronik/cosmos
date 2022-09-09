61b81e11ee0814773f7df1ff02be9b17578b8c45
Romchik <romankolesov44@gmail.com>
2022-09-09 23:32:55 +0500

    commit

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(".videocourse-card").click(function (e) {
+    e.preventDefault();
+    $(".overlay").show().css("overflow", "auto");
+    $("html").css("overflow-y", "hidden");
+    $(".modal-videocourse").animate({
+      top: "50%",
+    });
+  });
+
+  $(".overlay").click(function () {
+    $(".overlay").hide();
+    $("html").css("overflow-y", "scroll");
+    $(".modal-videocourse").animate({
+      top: "-150%",
+    });
+  });
+
+  $(".preview__buy-course").click(function (e) {
+    e.preventDefault();
+    $(".overlay").show().css("overflow", "auto");
+    $("html").css("overflow-y", "hidden");
+    $(".modal-manager").animate({
+      top: "50%",
+    });
+    $(".modal-videocourse").animate({
+      top: "-150%",
+    });
+  });
+
+  $(".overlay, .modal-manager__exit").click(function () {
+    $(".overlay").hide();
+    $("html").css("overflow-y", "scroll");
+    $(".modal-manager").animate({
+      top: "-100%",
+    });
+  });
+});
