ff76b5f0aa32156ad76793aa87bc56f3723a2818
Romchik <romankolesov44@gmail.com>
2022-09-13 12:28:45 +0500

    updated

--- js/modal.js
+++ js/modal.js
@@ -35,4 +35,45 @@ $(document).ready(function () {
       top: "-100%",
     });
   });
+
+  $(".tutors_search").click(function (e) {
+    e.preventDefault();
+    $(".header-search").animate(
+      {
+        top: 0,
+      },
+      800
+    );
+  });
+  $("#close").click(function (e) {
+    e.preventDefault();
+    $(".header-search").animate(
+      {
+        top: -100,
+      },
+      800
+    );
+  });
+
+  $(".show_hide-menu").click(function () {
+    if (!$(".filters").hasClass("open")) {
+      $(".filters").animate(
+        {
+          right: 0,
+        },
+        800
+      );
+      $(".filters").addClass("open");
+      $(".fa-sign-in").addClass("fa-rotate-270");
+    } else {
+      $(".filters").animate(
+        {
+          right: -300,
+        },
+        800
+      );
+      $(".filters").removeClass("open");
+      $(".fa-sign-in").removeClass("fa-rotate-270");
+    }
+  });
 });
